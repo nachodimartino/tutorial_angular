7a6333b65321e84005c34af53712279a7cb6a690
Ignacio Di Martino <nachojdimartino@gmail.com>
2024-09-20 12:52:23 -0300

    agrego

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroesComponent } from "./heroes/heroes.component";
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule, HeroesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-angular-project';
+  title = 'Tour Of Heroes';
 }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Hero } from '../hero';
+import { CommonModule } from '@angular/common';
+import {  FormsModule } from '@angular/forms';
+import { HEROES } from '../mock-heroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+
+@Component({
+  selector: 'app-heroes',
+  standalone: true,
+  imports: [CommonModule,FormsModule,HeroDetailComponent],
+  templateUrl: './heroes.component.html',
+  styleUrl: './heroes.component.css'
+})
+export class HeroesComponent {
+  
+  heroes=HEROES
+  selectedHero!: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
